02609158ebe744e071c234b4a24ae444db861a5f
Dhruv Sharma <dhruv2k24@gmail.com>
2023-01-20 10:16:53 +0530

    added

---
+++ src/Components/Testimonials.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+const Testimonials = ({ data }) => {
+  if (data) {
+    var testimonials = data.testimonials.map(function (testimonials) {
+      return (
+        <li key={testimonials.user}>
+          <blockquote>
+            <p>{testimonials.text}</p>
+            <h1>
+              <cite>{testimonials.user}</cite>
+              </h1>
+          </blockquote>
+        </li>
+      );
+    });
+  }
+
+  return (
+    <section id="testimonials">
+      <div className="text-container">
+        <div className="row">
+          <div className="two columns header-col">
+            <h1>
+              <span></span>
+            </h1>
+          </div>
+
+          <div className="ten columns flex-container">
+            <ul className="slides">{testimonials}</ul>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Testimonials;
